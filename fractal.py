b2b5deb52fbefa5eaf3dc0baa05c38308aa408ff
hjboss <hongjiangproject@gmail.com>
2021-04-14 20:16:37 +0800

    Initial commit

---
+++ fractal.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Copyright 2021 The HongJiang Library Authors. All right reserved.
+Use of this source that is governed by a Apache-style
+license that can be found in the LICENSE file.
+
+定義常見的 分形幾何圖(Fractal Geometry) 生成器
+
+@authors hjboss <hongjiangproject@gmail.com> 2021-04 $
+"""
+import math
+
+class FractalGeometry:
+  ''' Fractal Geometry Bitmap '''
